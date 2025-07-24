22c18ee8958ab59ed8743b238b312ffa53d793db
evgenii <evgenii@localhost.localdomain>
2025-07-24 19:20:45 +0300

    trash files

--- trash/beta.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$N = 10;
-
-for($i=0; $i<$N; $i++) {
-    exec("php alpha.php");
-}
-
-?>
--- trash/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Пример с exec()
-exec('php 1.php');
-
-// Пример с shell_exec()
-$output = shell_exec('php 2.php');
-echo $output;
-?>
